593482354a8b2d6d2a9d3ac7abe41d3efa6a2f85
Gavin Garza <gaving@live.com>
2023-06-14 22:31:31 +0900

    setup

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,92 +1,95 @@
-import { useState, useEffect } from 'react'
-import Note from './components/Note'
-import Notification from './components/Notification'
-import Footer from './components/Footer'
-import noteService from './services/notes'
+import "./styles.css";
+import React, { useState, useEffect } from "react";
+import Search from "./components/Search";
+import Phonebook from "./components/Phonebook";
+import Numbers from "./components/Numbers";
+import services from './services/persons'
 
-const App = () => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState(null)
+export default function App() {
+  const [usePeople, setPeople ] = useState([]);
+  const [newName, setName] = useState("");
+  const [newNumber, setNumber] = useState("");
+  const [useSearch, setSearch] = useState("");
+  const [useSearchResults, setSearchResults] = useState([]);
 
-  useEffect(() => {
-    noteService
-      .getAll()
-      .then(initialNotes => {
-        setNotes(initialNotes)
-      })
-  }, [])
+  const getPeople = () => {
+    services.getAll()
+     .then(people => {
+        console.log(people);
+        setPeople(people);
+      });
+  };
 
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      important: Math.random() > 0.5,
-    }
-
-    noteService
-      .create(noteObject)
-        .then(returnedNote => {
-        setNotes(notes.concat(returnedNote))
-        setNewNote('')
-      })
+  const postPeople = (name, number) => {
+    const id = usePeople.length + 1;
+    services.createNew(name, number, id)
   }
 
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value)
+  const updatePerson = (foundPerson) => {
+    services.updatePerson({ 
+      ...foundPerson,
+      number: newNumber,
+    })
+    formClear();
   }
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important)
-
-   const toggleImportanceOf = id => {
-      const note = notes.find(n => n.id === id)
-      const changedNote = { ...note, important: !note.important }
-  
-      noteService
-        .update(id, changedNote).then(returnedNote => {
-          setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-        })
-        .catch(error => {
-          setErrorMessage(
-            `Note '${note.content}' was already removed from server`
-          )
-          setTimeout(() => {
-            setErrorMessage(null)
-          }, 5000)
-          setNotes(notes.filter(n => n.id !== id))
-        })
+  const deleteOne = (id, name) => {
+    if(window.confirm(`Delete ${name}?`)) {
+      services.deletePerson(id);
+      getPeople();
     }
+  }
+
+  useEffect(getPeople, [])
+
+  const formClear = () => {
+    getPeople();
+    setName("");
+    setNumber("");
+  }
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    const filtered = usePeople.some((person) => person.name === newName);
+    if(filtered) {
+      const foundPerson = usePeople.find((person) => person.name === newName);
+      if(window.confirm(`${newName} has already been added to the phonebook, replace the old number with a new one?`)) {
+        updatePerson(foundPerson)
+      }
+    } else if (!filtered) {
+      postPeople(newName, newNumber);
+      formClear();
+    }
+  };
+
+  const getResults = () => {
+    const searchNames = usePeople.filter((person) => {
+      return person.name.toLowerCase().includes(useSearch.toLowerCase());
+    });
+    setSearchResults(searchNames);
+  };
 
   return (
-    <div>
-      <h1>Notes</h1>
-      <Notification message={errorMessage} />
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
-        </button>
-      </div> 
-      <ul>
-        <ul>
-          {notesToShow.map(note => 
-            <Note
-              key={note.id}
-              note={note}
-              toggleImportance={() => toggleImportanceOf(note.id)}
-            />
-          )}
-        </ul>
-      </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
-      <Footer />
-    </div>
-  )
-}
+    <div className="App">
+      <h2>Name search</h2>
+      <Search
+        getResults={getResults}
+        useSearch={useSearch}
+        setSearch={setSearch}
+        useSearchResults={useSearchResults}
+      ></Search>
 
-export default App
+      <h2>Phonebook</h2>
+      <Phonebook
+        newName={newName}
+        setName={setName}
+        newNumber={newNumber}
+        setNumber={setNumber}
+        addPerson={addPerson}
+      ></Phonebook>
+
+      <h2>Numbers</h2>
+      <Numbers usePeople={usePeople} deleteOne={deleteOne}></Numbers>
+    </div>
+  );
+}
---
+++ practice/src/services/persons.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const url = 'http://localhost:3001/api/persons';
+
+const getAll = () => {
+  const request = axios.get(url);
+  return request.then(res => res.data);
+};
+
+const createNew = (name, number, id) => {
+  return axios.post(url, {
+    name: name,
+    number: number,
+    id: id
+  }).catch(err => console.log(err))
+};
+
+const updatePerson = (foundPerson) => {
+  const request = axios.put(`${url}/${foundPerson.id}`, foundPerson)
+  return request.then(res => res.data)
+}
+
+const deletePerson = (id) => {
+   return axios.delete(`${url}/${id}`)
+  .catch(err => console.log(err))
+};
+
+export default { getAll, createNew, updatePerson, deletePerson }

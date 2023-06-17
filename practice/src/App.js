59c02cc6e7690d5c55a2318a1267a46ddb0cbc68
Gavin Garza <gaving@live.com>
2023-06-17 13:19:04 +0900

    added update method

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,73 +1,78 @@
-import "./styles.css";
-import React, { useState, useEffect } from "react";
-import Search from "./components/Search";
-import Phonebook from "./components/Phonebook";
-import Numbers from "./components/Numbers";
+import './styles.css'
+import React, { useState, useEffect } from 'react'
+import Search from './components/Search'
+import Phonebook from './components/Phonebook'
+import Numbers from './components/Numbers'
 import services from './services/persons'
 
 export default function App() {
-  const [usePeople, setPeople ] = useState([]);
-  const [newName, setName] = useState("");
-  const [newNumber, setNumber] = useState("");
-  const [useSearch, setSearch] = useState("");
-  const [useSearchResults, setSearchResults] = useState([]);
+  const [usePeople, setPeople] = useState([])
+  const [newName, setName] = useState('')
+  const [newNumber, setNumber] = useState('')
+  const [useSearch, setSearch] = useState('')
+  const [useSearchResults, setSearchResults] = useState([])
 
   const getPeople = () => {
-    services.getAll()
-     .then(people => {
-        console.log(people);
-        setPeople(people);
-      });
-  };
+    services.getAll().then((people) => {
+      console.log(people)
+      setPeople(people)
+    })
+  }
 
   const postPeople = (name, number) => {
-    const id = usePeople.length + 1;
+    const id = usePeople.length + 1
     services.createNew(name, number, id)
+    getPeople()
   }
 
   const updatePerson = (foundPerson) => {
-    services.updatePerson({ 
+    services.updatePerson({
       ...foundPerson,
       number: newNumber,
     })
-    formClear();
+    formClear()
   }
 
   const deleteOne = (id, name) => {
-    if(window.confirm(`Delete ${name}?`)) {
-      services.deletePerson(id);
-      getPeople();
+    if (window.confirm(`Delete ${name}?`)) {
+      services.deletePerson(id)
+      getPeople()
     }
   }
 
   useEffect(getPeople, [])
 
   const formClear = () => {
-    getPeople();
-    setName("");
-    setNumber("");
+    getPeople()
+    setName('')
+    setNumber('')
   }
 
   const addPerson = (e) => {
-    e.preventDefault();
-    const filtered = usePeople.some((person) => person.name === newName);
-    if(filtered) {
-      const foundPerson = usePeople.find((person) => person.name === newName);
-      if(window.confirm(`${newName} has already been added to the phonebook, replace the old number with a new one?`)) {
+    e.preventDefault()
+    const filtered = usePeople.some((person) => person.name === newName)
+    console.log(filtered);
+    if (filtered) {
+      const foundPerson = usePeople.find((person) => person.name === newName)
+      if (
+        window.confirm(
+          `${newName} has already been added to the phonebook, replace the old number with a new one?`
+        )
+      ) {
         updatePerson(foundPerson)
       }
     } else if (!filtered) {
-      postPeople(newName, newNumber);
-      formClear();
+      postPeople(newName, newNumber)
+      formClear()
     }
-  };
+  }
 
   const getResults = () => {
     const searchNames = usePeople.filter((person) => {
-      return person.name.toLowerCase().includes(useSearch.toLowerCase());
-    });
-    setSearchResults(searchNames);
-  };
+      return person.name.toLowerCase().includes(useSearch.toLowerCase())
+    })
+    setSearchResults(searchNames)
+  }
 
   return (
     <div className="App">
@@ -91,5 +96,5 @@ export default function App() {
       <h2>Numbers</h2>
       <Numbers usePeople={usePeople} deleteOne={deleteOne}></Numbers>
     </div>
-  );
+  )
 }
